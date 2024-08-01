67d6b950d6c706cd028d15819b015a344a9748a7
UshanKaushalya <Ushanwk22@gmail.com>
2024-08-01 12:49:39 +0530

    first commit

---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,9 @@
+import {SafeAreaView, Text} from "react-native";
+
+export default function WelcomeScreen(){
+    return(
+        <SafeAreaView>
+            <Text>Welcome Screen</Text>
+        </SafeAreaView>
+    )
+}
